d96a9185171ee831fa24a25e603a949257c8e3cb
Trent S Johnson <zookmunrig@gmail.com>
2021-06-17 14:00:25 -0400

    finished local ratio, lp, two round mathcing started: RLS

---
+++ lp.py
@@ -0,0 +1,48 @@
+import pulp
+
+class LP:
+    def euc_dis(self,x1,y1,x2,y2):
+        return ((x1-x2)**2+(y1-y2)**2)**.5
+    def edge_cost(self,edge, cwgraph):
+        return self.euc_dis(cwgraph.nodes[edge[2]]['x'], cwgraph.nodes[edge[2]]['y'], cwgraph.nodes[edge[0]]['xe'], cwgraph.nodes[edge[0]]['ye']) + self.euc_dis(cwgraph.nodes[edge[1]]['x'], cwgraph.nodes[edge[1]]['y'], cwgraph.nodes[edge[2]]['x'], cwgraph.nodes[edge[2]]['y']) + self.euc_dis(cwgraph.nodes[edge[1]]['x'], cwgraph.nodes[edge[1]]['y'], cwgraph.nodes[edge[0]]['xs'], cwgraph.nodes[edge[0]]['ys'])
+
+    def total_cost(self,matching, cwgraph):
+        return sum([self.edge_cost(edge,cwgraph) for edge in matching])
+    
+    def solve(self,cwgraph,worker,overflow,underflow):
+        #define arrangements
+        possible_edges= [(w,o,u) for w in worker for o in overflow for u in underflow]
+
+        #make problem 
+        wap_model = pulp.LpProblem("WAP_Model", pulp.LpMinimize)
+
+        #make dict of them with bounds for integer assignments
+        x = pulp.LpVariable.dicts(
+            "edge_fractions", possible_edges, lowBound=0, upBound=1, cat=pulp.LpInteger
+        )
+
+        #create objective function
+        wap_model += pulp.lpSum([x[edge]*self.edge_cost(edge,cwgraph) for edge in possible_edges])
+
+        #contrain weights to sum to 1
+        for vertex in list(worker):
+            wap_model += (
+                pulp.lpSum([x[edge] for edge in possible_edges if vertex in edge]) == 1,
+                "assignment_bound_%s" % str(vertex),
+            )
+
+        for vertex in overflow:
+            wap_model += (
+                pulp.lpSum([x[edge] for edge in possible_edges if vertex in edge]) <= 1,
+                "assignment_bound_%s" % str(vertex),
+            )
+        for vertex in underflow:
+            wap_model += (
+                pulp.lpSum([x[edge] for edge in possible_edges if vertex in edge]) <= 1,
+                "assignment_bound_%s" % str(vertex),
+            )
+        wap_model.solve()
+        print("Status:", pulp.LpStatus[wap_model.status])
+
+        matching = [edge for edge in possible_edges if x[edge].value() > 0]
+        return matching, self.total_cost(matching,cwgraph)
---
+++ trm.py
@@ -0,0 +1,49 @@
+from networkx.algorithms import bipartite
+import networkx as nx
+
+class TRM:
+    def euc_tri(self, worker, underflow, overflow, graph):
+        worker_data = graph.nodes[worker]
+        underflow_data = graph.nodes[underflow]
+        overflow_data = graph.nodes[overflow]
+        return self.euc_dis(worker_data['xe'],worker_data['ye'],underflow_data['x'],underflow_data['y']) + self.euc_dis(worker_data['xs'],worker_data['ys'],overflow_data['x'],overflow_data['y']) + self.euc_dis(underflow_data['x'],underflow_data['y'],overflow_data['x'],overflow_data['y'])
+
+    def matching_score(self,matching,graph):
+        triplets = []
+        finished = {}
+        for u1 in matching.keys():
+            temp = {}
+            if type(u1)==str:
+                u2 = matching[u1]
+                u1,u3 = u1.split('|')
+            else:
+                u2 = matching[u1]
+                u2,u3 = u2.split('|')
+            temp[graph.nodes[u1]['type']] = u1
+            temp[graph.nodes[u2]['type']] = u2
+            temp[graph.nodes[u3]['type']] = u3
+            triplets.append(temp)
+        w = 0
+        for i in triplets:
+            w += self.euc_tri(i['worker'],i['underflow'],i['overflow'],graph)
+        return triplets,w
+    
+    def euc_dis(self,x1,y1,x2,y2):
+        return ((x1-x2)**2+(y1-y2)**2)**.5
+
+    def solve(self, cgraph,cwgraph, worker):
+        matching = bipartite.matching.minimum_weight_full_matching(cgraph)
+        spwgraph =nx.Graph()
+        for key in matching.keys():
+            if cgraph.nodes[key]['type'] =='overflow':
+                for w, worker_ in enumerate(worker):
+                    spwgraph.add_edge((key+'|'+matching[key]),w,
+                                    weight=cgraph.edges[key, matching[key]]['weight']+
+                                    self.euc_dis(cwgraph.nodes[worker_]['xs'], cwgraph.nodes[worker_]['ys'],
+                                     cgraph.nodes[key]['x'], cgraph.nodes[key]['y']) +
+                                    self.euc_dis(cwgraph.nodes[worker_]['xe'], cwgraph.nodes[worker_]['ye'], 
+                                    cgraph.nodes[matching[key]]['x'], cgraph.nodes[matching[key]]['y'])
+                                                )
+        matching2 = bipartite.matching.minimum_weight_full_matching(spwgraph)
+        #print(matching2)
+        return self.matching_score(matching2, cwgraph)
