59854edc8aa83c191cb876caa50ffdccdfdec4bf
Trent S Johnson <zookmunrig@gmail.com>
2021-06-21 10:32:24 -0400

    maybe fixed?

--- models/trm.py
+++ models/trm.py
@@ -2,66 +2,74 @@ from networkx.algorithms import bipartite
 import networkx as nx
 from networkx.algorithms.shortest_paths import weighted
 
+
 class TRM:
     def euc_tri(self, worker, underflow, overflow, graph):
         worker_data = graph.nodes[worker]
         underflow_data = graph.nodes[underflow]
         overflow_data = graph.nodes[overflow]
-        return self.euc_dis(worker_data['xe'],worker_data['ye'],underflow_data['x'],underflow_data['y']) + self.euc_dis(worker_data['xs'],worker_data['ys'],overflow_data['x'],overflow_data['y']) + self.euc_dis(underflow_data['x'],underflow_data['y'],overflow_data['x'],overflow_data['y'])
+        return self.euc_dis(worker_data['xe'], worker_data['ye'], underflow_data['x'],
+                            underflow_data['y']) + self.euc_dis(worker_data['xs'], worker_data['ys'],
+                                                                overflow_data['x'], overflow_data['y']) + self.euc_dis(
+            underflow_data['x'], underflow_data['y'], overflow_data['x'], overflow_data['y'])
 
-    def matching_score(self,matching,graph):
+    def matching_score(self, matching, graph):
         triplets = []
         for u1 in matching.keys():
             temp = {}
             if '|' in u1:
                 u2 = matching[u1]
-                u1,u3 = u1.split('|')
+                u1, u3 = u1.split('|')
             else:
                 u2 = matching[u1]
-                u2,u3 = u2.split('|')
+                u2, u3 = u2.split('|')
             temp[graph.nodes[u1]['type']] = u1
             temp[graph.nodes[u2]['type']] = u2
             temp[graph.nodes[u3]['type']] = u3
             triplets.append(temp)
         w = 0
         for i in triplets:
-            w += self.euc_tri(i['worker'],i['underflow'],i['overflow'],graph)
-        return triplets,w
-    
-    def euc_dis(self,x1,y1,x2,y2):
-        return ((x1-x2)**2+(y1-y2)**2)**.5
+            w += self.euc_tri(i['worker'], i['underflow'], i['overflow'], graph)
+        return triplets, w
 
-    def solve(self, cgraph,cwgraph, worker):
+    def euc_dis(self, x1, y1, x2, y2):
+        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** .5
+
+    def solve(self, cgraph, cwgraph, worker):
         matching = bipartite.matching.minimum_weight_full_matching(cgraph)
-        spwgraph =nx.Graph()
+        spwgraph = nx.Graph()
         for key in matching.keys():
-            if cgraph.nodes[key]['type'] =='overflow':
-                #print('is overflow')
+            if cgraph.nodes[key]['type'] == 'overflow':
+                # print('is overflow')
                 for w, worker_ in enumerate(worker):
-                    spwgraph.add_edge((key+'|'+matching[key]),worker_,
-                                    weight=self.euc_dis(cwgraph.nodes[key]['x'], cwgraph.nodes[key]['y'],
-                                     cgraph.nodes[matching[key]]['x'], cgraph.nodes[matching[key]]['y'])+
-                                    self.euc_dis(cwgraph.nodes[worker_]['xs'], cwgraph.nodes[worker_]['ys'],
-                                     cgraph.nodes[key]['x'], cgraph.nodes[key]['y']) +
-                                    self.euc_dis(cwgraph.nodes[worker_]['xe'], cwgraph.nodes[worker_]['ye'], 
-                                    cgraph.nodes[matching[key]]['x'], cgraph.nodes[matching[key]]['y'])
-                                                )
-        #print('spw',spwgraph.edges)
+                    spwgraph.add_edge((key + '|' + matching[key]), worker_,
+                                      weight=self.euc_dis(cwgraph.nodes[key]['x'], cwgraph.nodes[key]['y'],
+                                                          cgraph.nodes[matching[key]]['x'],
+                                                          cgraph.nodes[matching[key]]['y']) +
+                                             self.euc_dis(cwgraph.nodes[worker_]['xs'], cwgraph.nodes[worker_]['ys'],
+                                                          cgraph.nodes[key]['x'], cgraph.nodes[key]['y']) +
+                                             self.euc_dis(cwgraph.nodes[worker_]['xe'], cwgraph.nodes[worker_]['ye'],
+                                                          cgraph.nodes[matching[key]]['x'],
+                                                          cgraph.nodes[matching[key]]['y'])
+                                      )
+        # print('spw',spwgraph.edges)
         matching2 = bipartite.matching.minimum_weight_full_matching(spwgraph)
-        #print(matching2)
+        # print(matching2)
         return self.matching_score(matching2, cwgraph)
 
-    def test(self,cwgraph):
+    def test(self, cwgraph):
         cgraph = nx.Graph()
-        cgraph.add_nodes_from([node for node in cwgraph.nodes if cwgraph.nodes[node]['type']!='worker'])
-        nx.set_node_attributes(cgraph, {i: cwgraph.nodes[i] for i in cgraph.nodes}) 
+        cgraph.add_nodes_from([node for node in cwgraph.nodes if cwgraph.nodes[node]['type'] != 'worker'])
+        nx.set_node_attributes(cgraph, {i: cwgraph.nodes[i] for i in cgraph.nodes})
 
-        worker =list(set(cwgraph.nodes)-set(cgraph.nodes))
+        worker = list(set(cwgraph.nodes) - set(cgraph.nodes))
         for edge in cwgraph.edges:
             if cgraph.has_node(edge[0]) and cgraph.has_node(edge[1]):
-                cgraph.add_edge(edge[0],edge[1],weight=self.euc_dis(cwgraph.nodes[edge[0]]['x'],cwgraph.nodes[edge[0]]['y'],cwgraph.nodes[edge[1]]['x'],cwgraph.nodes[edge[1]]['y']))
-        #print('nodedata',cgraph.nodes[list(cgraph.nodes)[0]])
-        #worker = [node for node in cwgraph.nodes if cwgraph.nodes[node]['type']=='worker']
-        #print(len(worker))
+                cgraph.add_edge(edge[0], edge[1],
+                                weight=self.euc_dis(cwgraph.nodes[edge[0]]['x'], cwgraph.nodes[edge[0]]['y'],
+                                                    cwgraph.nodes[edge[1]]['x'], cwgraph.nodes[edge[1]]['y']))
+        # print('nodedata',cgraph.nodes[list(cgraph.nodes)[0]])
+        # worker = [node for node in cwgraph.nodes if cwgraph.nodes[node]['type']=='worker']
+        # print(len(worker))
 
-        return self.solve(cgraph,cwgraph,worker)
+        return self.solve(cgraph, cwgraph, worker)
--- models/uga.py
+++ models/uga.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import networkx as nx
 from scipy.special import softmax
@@ -7,199 +6,201 @@ from random import shuffle
 import copy
 from models.hs import RSL
 
+
 class UGA:
-    def __init__(self,meta_graph=None):
+    def __init__(self, meta_graph=None):
         self.meta_graph = meta_graph
+
     def build_lists(self):
         self.overflow = [node for node in self.meta_graph.nodes() if self.meta_graph.nodes[node]['type'] == 'overflow']
         self.worker = [node for node in self.meta_graph.nodes() if self.meta_graph.nodes[node]['type'] == 'worker']
-        self.underflow = [node for node in self.meta_graph.nodes() if self.meta_graph.nodes[node]['type'] == 'underflow']
-        
+        self.underflow = [node for node in self.meta_graph.nodes() if
+                          self.meta_graph.nodes[node]['type'] == 'underflow']
+
     def add_triplet(self, vertex, graph):
         neighbors = list(self.meta_graph.neighbors(vertex))
         shuffle(neighbors)
         overflow = None
         underflow = None
         for overflow_ in neighbors:
-            if (not overflow_ in list(graph.nodes())) and self.meta_graph.nodes[overflow_]['type']=='overflow':
-                
+            if (not overflow_ in list(graph.nodes())) and self.meta_graph.nodes[overflow_]['type'] == 'overflow':
                 overflow = overflow_
         for underflow_ in neighbors:
-            if (not underflow_ in list(graph.nodes())) and self.meta_graph.nodes[underflow_]['type']=='underflow':
+            if (not underflow_ in list(graph.nodes())) and self.meta_graph.nodes[underflow_]['type'] == 'underflow':
                 underflow = underflow_
-        if overflow != None and underflow != None:    
+        if overflow != None and underflow != None:
             graph.add_node(overflow)
             graph.add_node(underflow)
             graph.add_node(vertex)
-            graph.add_edge(vertex,underflow)
-            graph.add_edge(vertex,overflow)
-            
-    def euc_dis(self,x1,y1,x2,y2):
-        return ((x1-x2)**2+(y1-y2)**2)**.5
-            
-    #each species is a complete of worker station triples
+            graph.add_edge(vertex, underflow)
+            graph.add_edge(vertex, overflow)
+
+    def euc_dis(self, x1, y1, x2, y2):
+        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** .5
+
+    # each species is a complete of worker station triples
     def build_matching(self):
         graph = nx.Graph()
-        
+
         shuffle(self.worker)
         for w in self.worker:
-            self.add_triplet(w,graph)
+            self.add_triplet(w, graph)
         graph.add_nodes_from([i for i in self.meta_graph.nodes if not i in list(graph.nodes)])
         return graph
-    
-    def find_overflow(self,vertex):
+
+    def find_overflow(self, vertex):
         while True:
             node_ind = np.random.choice(range(len(self.overflow)))
             if self.overflow[node_ind] != vertex:
                 return self.overflow[node_ind]
-            
-    def find_underflow(self,vertex):
+
+    def find_underflow(self, vertex):
         while True:
             node_ind = np.random.choice(range(len(self.underflow)))
             if self.underflow[node_ind] != vertex:
                 return self.underflow[node_ind]
-            
-    
+
     def mutate(self, worker, to_replace, graph):
         if self.meta_graph.nodes[to_replace]['type'] == 'overflow':
             other = self.find_overflow(to_replace)
         else:
             other = self.find_underflow(to_replace)
-        if len(list(graph.neighbors(other)))>0:
+        if len(list(graph.neighbors(other))) > 0:
             otherw = list(graph.neighbors(other))[0]
-            
-            graph.add_edge(otherw,to_replace)
-            graph.remove_edge(otherw,other)
-        graph.add_edge(worker,other)
-        graph.remove_edge(worker,to_replace)
-            
-    def pick_edge(self,ref_station,edges):
+
+            graph.add_edge(otherw, to_replace)
+            graph.remove_edge(otherw, other)
+        graph.add_edge(worker, other)
+        graph.remove_edge(worker, to_replace)
+
+    def pick_edge(self, ref_station, edges):
         for edge in edges:
             for vertex in edges:
                 if self.meta_graph.nodes[vertex]['type'] == self.meta_graph.nodes[ref_station]['type']:
                     return edge
-        
-    
-    def euc_fitness_ind(self,gene,spec):
+
+    def euc_fitness_ind(self, gene, spec):
         w = 0
         neighbors = list(spec.neighbors(gene))
         if len(neighbors) != 0:
             for i in neighbors:
                 if self.meta_graph.nodes[i]['type'] == 'underflow':
                     w += self.euc_dis(self.meta_graph.nodes[i]['x'], self.meta_graph.nodes[i]['y'],
-                                 self.meta_graph.nodes[gene]['xs'],self.meta_graph.nodes[gene]['ys'])
+                                      self.meta_graph.nodes[gene]['xs'], self.meta_graph.nodes[gene]['ys'])
                 else:
-                    w += self.euc_dis(self.meta_graph.nodes[i]['x'],self.meta_graph.nodes[i]['y'],
-                                 self.meta_graph.nodes[gene]['xe'],self.meta_graph.nodes[gene]['ye'])
-            w += self.euc_dis(self.meta_graph.nodes[neighbors[0]]['x'],self.meta_graph.nodes[neighbors[0]]['y'],
-                         self.meta_graph.nodes[neighbors[1]]['x'],self.meta_graph.nodes[neighbors[1]]['y'])
+                    w += self.euc_dis(self.meta_graph.nodes[i]['x'], self.meta_graph.nodes[i]['y'],
+                                      self.meta_graph.nodes[gene]['xe'], self.meta_graph.nodes[gene]['ye'])
+            w += self.euc_dis(self.meta_graph.nodes[neighbors[0]]['x'], self.meta_graph.nodes[neighbors[0]]['y'],
+                              self.meta_graph.nodes[neighbors[1]]['x'], self.meta_graph.nodes[neighbors[1]]['y'])
 
         return w
-    
-    def euc_fitness(self,spec):
-        return -sum([self.euc_fitness_ind(gene,spec) for gene in spec.nodes if self.meta_graph.nodes[gene]['type'] == 'worker'])
 
-    def swap(self,worker,station,mate_station,spec):
-        #print('worker',worker,'station',station,'mate_station',mate_station)
-        #print('removed',worker,station)
-        spec.remove_edge(worker,station)
+    def euc_fitness(self, spec):
+        return -sum([self.euc_fitness_ind(gene, spec) for gene in spec.nodes if
+                     self.meta_graph.nodes[gene]['type'] == 'worker'])
+
+    def swap(self, worker, station, mate_station, spec):
+        # print('worker',worker,'station',station,'mate_station',mate_station)
+        # print('removed',worker,station)
+        spec.remove_edge(worker, station)
         if len(list(spec.neighbors(mate_station))) > 0:
             unemployed = list(spec.neighbors(mate_station))[0]
-            #print('mate_neighbors:', list(spec.neighbors(mate_station)))
-            #print('worker:', worker)
-            #print('unemplowed:',unemployed)
-            spec.remove_edge(unemployed,mate_station)
-            #print('removed',unemployed,mate_station)
-            #print('added',worker,mate_station)
-            spec.add_edge(worker,mate_station)
+            # print('mate_neighbors:', list(spec.neighbors(mate_station)))
+            # print('worker:', worker)
+            # print('unemplowed:',unemployed)
+            spec.remove_edge(unemployed, mate_station)
+            # print('removed',unemployed,mate_station)
+            # print('added',worker,mate_station)
+            spec.add_edge(worker, mate_station)
             pots = self.overflow if self.meta_graph.nodes[station]['type'] == 'overflow' else self.underflow
             shuffle(pots)
             for p in pots:
                 if len(list(spec.neighbors(p))) == 0:
-                    spec.add_edge(unemployed,p)
+                    spec.add_edge(unemployed, p)
                     return
             print('NoStationFound')
         else:
-            spec.add_edge(worker,mate_station)
-            #print('added',worker,mate_station)
-        
+            spec.add_edge(worker, mate_station)
+            # print('added',worker,mate_station)
 
-
-            
     def run(self, sswap_rate, oswap_rate, gens, pop_size, update_flag=False):
-        #make a population
+        # make a population
         self.build_lists()
         pop = [self.build_matching() for i in range(pop_size)]
-        
+
         bests = []
         for gen in range(gens):
-            #get scores of species
+            # get scores of species
             scores = np.array([self.euc_fitness(s) for s in pop])
             bests.append(max(scores))
-            #if gen %50 ==0 and update_flag:
-                #print(min(scores),max(scores))
-            scores = softmax(scores)  
-            
+            # if gen %50 ==0 and update_flag:
+            # print(min(scores),max(scores))
+            scores = softmax(scores)
+
             selected = []
-            i=0
+            i = 0
             distr = []
-            #while selecting pick a species
+            # while selecting pick a species
             while len(selected) < len(pop):
                 spec_ind = np.random.choice(list(range(len(pop))), p=scores)
-                #spec = pop[i]
-                
+                # spec = pop[i]
+
                 distr.append(spec_ind)
                 spec_o = pop[spec_ind]
                 spec = copy.deepcopy(spec_o)
-                #for each gene (a triple)
-                for j,node in enumerate(spec.nodes):
-                    #swap with another pop
+                # for each gene (a triple)
+                for j, node in enumerate(spec.nodes):
+                    # swap with another pop
                     edges = set(spec.edges)
-                    if self.meta_graph.nodes[node]['type']!= 'worker' and len(list(spec.neighbors(node)))>0 and np.random.random() < sswap_rate:
+                    if self.meta_graph.nodes[node]['type'] != 'worker' and len(
+                            list(spec.neighbors(node))) > 0 and np.random.random() < sswap_rate:
                         self.mutate(list(spec.neighbors(node))[0], node, spec)
-                        #print(edges.intersection(set(self.meta_graph.edges).intersection(set(spec.edges))))
+                        # print(edges.intersection(set(self.meta_graph.edges).intersection(set(spec.edges))))
                 for edge in spec.edges:
                     edge = list(edge)
                     if edge[0] in self.worker:
                         worker_, station_ = edge[0], edge[1]
                     else:
-                         worker_, station_ =edge[1], edge[0]
+                        worker_, station_ = edge[1], edge[0]
 
                     if np.random.random() < oswap_rate:
-
                         mate_stations_ = list(pop[np.random.choice(list(range(len(pop))), p=scores)].neighbors(worker_))
-                        mate_station_  = mate_stations_[0] if self.meta_graph.nodes[mate_stations_[0] ]['type'] == self.meta_graph.nodes[station_ ]['type'] else mate_stations_[1]
-                        self.swap(worker_, station_ , mate_station_ ,spec)
+                        mate_station_ = mate_stations_[0] if self.meta_graph.nodes[mate_stations_[0]]['type'] == \
+                                                             self.meta_graph.nodes[station_]['type'] else \
+                        mate_stations_[1]
+                        self.swap(worker_, station_, mate_station_, spec)
 
                 selected.append(spec)
-                                    
+
             pop = selected
-            #print(distr)
-        return pop[np.argmax(scores)],np.max(bests), bests
-                
-                
-    def test(self,cwgraph):
-        self.meta_graph=cwgraph
-        return self.run( 0.1, 0.1, 100, 100) 
+            # print(distr)
+        return pop[np.argmax(scores)], np.max(bests), bests
+
+    def test(self, cwgraph):
+        self.meta_graph = cwgraph
+        return self.run(0.1, 0.1, 100, 100)
+
 
 class UGA_RSL(UGA):
-    def __init__(self,meta_graph=None):
+    def __init__(self, meta_graph=None):
         super().__init__(meta_graph)
         self.rsl = RSL()
+
     def build_matching(self):
-        #print('g')
+        # print('g')
         graph = self.rsl.optimize(self.meta_graph)[0]
         graph.add_nodes_from([node for node in self.meta_graph.nodes if not graph.has_node(node)])
         for v1 in graph.nodes():
             for v2 in graph.nodes:
-                if graph.has_edge(v1,v2) and set([self.meta_graph.nodes[v1]['type'],self.meta_graph.nodes[v2]['type']]) == set(['overflow','underflow']):
-                    graph.remove_edge(v1,v2)
+                if graph.has_edge(v1, v2) and set(
+                        [self.meta_graph.nodes[v1]['type'], self.meta_graph.nodes[v2]['type']]) == set(
+                        ['overflow', 'underflow']):
+                    graph.remove_edge(v1, v2)
         return graph
-    
-    def run(self, oswap_rate, gens, pop_size):
-        return super().run(0,oswap_rate, gens, pop_size)
-    
-    def test(self, cwgraph):
-        self.meta_graph=cwgraph
-        return self.run( 0.1 , 5, 5) 
 
+    def run(self, oswap_rate, gens, pop_size):
+        return super().run(0, oswap_rate, gens, pop_size)
+
+    def test(self, cwgraph):
+        self.meta_graph = cwgraph
+        return self.run(0.1, 5, 5)
